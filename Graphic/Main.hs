a7780be801751b7d991b4b73896863b8cd0c99a9
Hawzen <43524721+Hawzen@users.noreply.github.com>
2020-06-19 16:24:46 +0300

    Add graphic

---
+++ Graphic/Main.hs
@@ -0,0 +1,21 @@
+module Main where
+
+main :: IO ()
+main = putStrLn "Hello, dminuoso!"
+
+data Store s a = Store s (s -> a)
+
+seek :: s -> Store s a -> Store s a
+seek s (Store _ a) = Store s a
+
+pos :: Store s a -> s
+pos (Store s a) = s
+
+peek :: s -> Store s a -> a
+peek s (Store _ a) = a s
+
+seeks :: (s -> s) -> Store s a -> Store s a
+seeks fs (Store s a) = Store (fs s) a
+
+peeks :: (s -> s) -> Store s a -> a
+peeks f (Store s a) = a $ f s
