8fa4d2c9355449c80293f88b231a0b358bfd2288
Hawzen <hawzen@users.noreply.github.com>
2022-04-03 22:56:43 +0300

    Revert binaries

--- rust/target/debug/deps/rust-ed7942381c4c739f.d
+++
@@ -1,5 +0,0 @@
-D:\Repositories\Experiments\rust\target\debug\deps\rust-ed7942381c4c739f.exe: src\main.rs
-
-D:\Repositories\Experiments\rust\target\debug\deps\rust-ed7942381c4c739f.d: src\main.rs
-
-src\main.rs:
--- rust/target/debug/rust.d
+++
@@ -1,1 +0,0 @@
-D:\Repositories\Experiments\rust\target\debug\rust.exe: D:\Repositories\Experiments\rust\src\main.rs
